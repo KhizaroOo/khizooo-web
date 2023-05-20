0cbd766db495d83e6e16516c27f98f2487418c0a
khizooo <khizar.imtiaz@vaival.com>
2023-05-20 18:56:41 +0500

    1st

---
+++ External/Arts.js
@@ -0,0 +1,67 @@
+window.MyArts = `{
+				    "MyArts": {
+				        "Title": "My Arts",
+				 	"Types": [
+				            {
+						"ID": 1,
+				                "Title": "Knife Paintings",
+				                "SI": "https://placeimg.com/640/480/arch",
+				                "MI": "https://placeimg.com/640/480/arch",
+		   	                       "LI": "https://placeimg.com/640/480/arch",
+				                "SD": "Art SD",
+				                "LD": "Art LD"
+				            },
+				            {
+						"ID": 2,
+				                "Title": "Sticky Notes Drawing",
+				                "SI": "https://placeimg.com/640/480/arch",
+				                "MI": "https://placeimg.com/640/480/arch",
+		   	                      "LI": "https://placeimg.com/640/480/arch",
+				                "SD": "Art SD",
+				                "LD": "Art LD"
+				            }
+					],
+				        "Data": [
+				            {
+						"ID": 1,
+				                "Title": "Big Bang Theory",
+				                "SI": "1/Big-Bang-Theory-Image.avif",
+				                "MI": "1/Big-Bang-Theory-Image.avif",
+		   	                      "LI": "1/Big-Bang-Theory-Image.avif",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "1/Big-Bang-Theory.html"
+				            },
+				{
+						"ID": 2,
+				                "Title": "Labor Day",
+				                "SI": "2/Labor-Day.png",
+				                "MI": "2/Labor-Day.png",
+		   	                      "LI": "2/Labor-Day.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "2/Labor-Day.html"
+				            },
+				{
+						"ID": 3,
+					        "Title": "Upcomings",
+				                "SI": "../assets/img/Upcoming.png",
+				                "MI": "../assets/img/Upcoming.png",
+		   	                        "LI": "../assets/img/Upcoming.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "http://khizooo.com/arts/1/Big-Bang-Theory.html"
+				            },
+				{
+						"ID": 4,
+				                "Title": "Upcomings",
+				                "SI": "../assets/img/Upcoming.png",
+				                "MI": "../assets/img/Upcoming.png",
+		   	                        "LI": "../assets/img/Upcoming.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "http://khizooo.com/arts/1/Big-Bang-Theory.html"
+				            }
+				        ]
+				    }
+				}`;
---
+++ External/Tech.js
@@ -0,0 +1,168 @@
+window.MyTech = `{
+				    "MyTech": {
+				        "Title": "My Tech",
+				 	"Types": [
+				            {
+						"ID": 1,
+				                "Title": "BlockChain",
+				                "SI": "https://placeimg.com/640/480/arch",
+				                "MI": "https://placeimg.com/640/480/arch",
+		   	                        "LI": "https://placeimg.com/640/480/arch",
+				                "SD": "BlockChain",
+				                "LD": "BlockChain"
+				            },
+				            {
+						"ID": 2,
+				                "Title": ".NET Technologies",
+				                "SI": "https://placeimg.com/640/480/arch",
+				                "MI": "https://placeimg.com/640/480/arch",
+		   	                        "LI": "https://placeimg.com/640/480/arch",
+				                "SD": ".NET Technologies",
+				                "LD": ".NET Technologies"
+				            }
+					],
+				        "Data": [
+				            {
+						"ID": 1,
+				                "Title": "What is BlockChain?",
+				                "SI": "1/What-is-BlockChain.png",
+				                "MI": "1/What-is-BlockChain.png",
+		   	                        "LI": "1/What-is-BlockChain.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "1/What-Is-BlockChain.html"
+				            },
+				            {
+						"ID": 2,
+				                "Title": "Blockchain - Decentralization",
+				                "SI": "2/BlockChain-Decentralization.png",
+				                "MI": "2/BlockChain-Decentralization.png",
+		   	                        "LI": "2/BlockChain-Decentralization.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "2/BlockChain-Decentralization.html"
+				            },
+					   {
+						"ID": 3,
+   						"Title": "Why Blockchain?",
+				              "SI": "3/Why-BlockChain.png",
+				                "MI": "3/Why-BlockChain.png",
+		   	                        "LI": "3/Why-BlockChain.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					       "DetailLink": "3/Why-BlockChain.html"
+				            },
+					   {
+						"ID": 4,
+				                "Title": "Blockchain Structure",
+				                 "SI": "4/BlockChain-Structure.png",
+				                "MI": "4/BlockChain-Structure.png",
+		   	                        "LI": "4/BlockChain-Structure.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					      "DetailLink": "4/BlockChain-Structure.html"
+				            },
+					   {
+						"ID": 5,
+				                "Title": "Basic Blockchain Operations",
+				                 "SI": "5/MainIMG.png",
+				                "MI": "5/MainIMG.png",
+		   	                        "LI":"5/MainIMG.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					      "DetailLink": "5/Basic-Blockchain-Operations.html"
+				            },
+					   {
+						"ID": 6,
+				                "Title": "Applications & Uses of Blockchain",
+				                 "SI": "6/Application-And-Uses-Of-Blockchain-Technology.png",
+				                "MI": "6/Application-And-Uses-Of-Blockchain-Technology.png",
+		   	                        "LI":"6/Application-And-Uses-Of-Blockchain-Technology.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					      "DetailLink": "6/Application-And-Uses-Of-Blockchain-Technology.html"
+				            },
+					   {
+						"ID": 7,
+				                "Title": "Top 10 Real-World Applications Of Blockchain",
+				                 "SI": "7/Top-10-Real-World-Applications-Of-Blockchain.png",
+				                "MI": "7/Top-10-Real-World-Applications-Of-Blockchain.png",
+		   	                        "LI":"7/Top-10-Real-World-Applications-Of-Blockchain.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					      "DetailLink": "7/Top-10-Real-World-Applications-Of-Blockchain.html"
+				            },
+					   {
+						"ID": 8,
+				                "Title": "Blockchain Architecture - Components & Benefits",
+				                 "SI": "8/Blockchain-Architecture-Components-And-Benefits.png",
+				                "MI": "8/Blockchain-Architecture-Components-And-Benefits.png",
+		   	                        "LI": "8/Blockchain-Architecture-Components-And-Benefits.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					      "DetailLink": "8/Blockchain-Architecture-Components-And-Benefits.html"
+				            },
+					   {
+						"ID": 9,
+				                "Title": "Blockchain General Knowledge",
+				                 "SI": "9/Blockchain-General-Knowledge.png",
+				                "MI": "9/Blockchain-General-Knowledge.png",
+		   	                        "LI": "9/Blockchain-General-Knowledge.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					      "DetailLink": "9/Blockchain-General-Knowledge.html"
+				            },
+					   {
+						"ID": 10,
+				                "Title": "Blockchain - Storage",
+				                 "SI": "10/Blockchain-Storage.png",
+				                "MI": "10/Blockchain-Storage.png",
+		   	                        "LI": "10/Blockchain-Storage.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					      "DetailLink": "10/Blockchain-Storage.html"
+				            },
+					   {
+						"ID": 11,
+				                "Title": "Blockchain - Cryptography",
+				                 "SI": "11/Blockchain-Cryptography.png",
+				                "MI": "11/Blockchain-Cryptography.png",
+		   	                        "LI": "11/Blockchain-Cryptography.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					      "DetailLink": "11/Blockchain-Cryptography.html"
+				            },
+					   {
+						"ID": 12,
+				                "Title": "Blockchain - Mining & Incentive Models",
+				                 "SI": "12/Blockchain-Mining-And-Incentive-Models.png",
+				                "MI": "12/Blockchain-Mining-And-Incentive-Models.png",		   	                        "LI": "12/Blockchain-Mining-And-Incentive-Models.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					      "DetailLink": "12/Blockchain-Mining-And-Incentive-Models.html"
+				            },
+					   {
+						"ID": 13,
+				                "Title": "Blockchain - Decentralized vs Trust",
+				                 "SI": "13/Blockchain-Decentralized-vs-Trust.png",
+				                "MI": "13/Blockchain-Decentralized-vs-Trust.png",		   	                        "LI": "13/Blockchain-Decentralized-vs-Trust.png",
+				                "SD": "Art SD",
+				                "LD": "Art LD",
+					      "DetailLink": "13/Blockchain-Decentralized-vs-Trust.html"
+				            },
+				{
+						"ID": 14,
+				                "Title": "Blockchain - Forking",
+				                 "SI": "14/Blockchain-Forking.png",
+					         "DetailLink": "14/Blockchain-Forking.html"
+				            },
+					   {
+						"ID": 15,
+				                "Title": "Blockchain - Consensus Protocols",
+				                 "SI": "15/Blockchain-Consensus-Protocols.png",
+					         "DetailLink": "15/Blockchain-Consensus-Protocols.html"
+				            }	
+																	
+				        ]
+				    }
+				}`;
